ea1f3baa1c0203cf4037a9db8335c361bb42eb74
StellarShine <gonghui.li106@gmail.com>
2015-10-14 23:24:59 +0800

    Update .sql文件
    合并表2和表3之后的.sql文件

---
+++ 数据库/Dump20151014.sql
@@ -0,0 +1,233 @@
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cooperate_fly
+-- ------------------------------------------------------
+-- Server version	5.6.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `catalog_table`
+--
+
+DROP TABLE IF EXISTS `catalog_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `catalog_table` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `catalog_table`
+--
+
+LOCK TABLES `catalog_table` WRITE;
+/*!40000 ALTER TABLE `catalog_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `catalog_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `data_info`
+--
+
+DROP TABLE IF EXISTS `data_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `data_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `type` int(11) NOT NULL,
+  `package_id` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data_info`
+--
+
+LOCK TABLES `data_info` WRITE;
+/*!40000 ALTER TABLE `data_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `data_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `data_value`
+--
+
+DROP TABLE IF EXISTS `data_value`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `data_value` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `version_id` int(11) NOT NULL,
+  `data_info_id` int(11) NOT NULL,
+  `value` mediumtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data_value`
+--
+
+LOCK TABLES `data_value` WRITE;
+/*!40000 ALTER TABLE `data_value` DISABLE KEYS */;
+/*!40000 ALTER TABLE `data_value` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `model_info`
+--
+
+DROP TABLE IF EXISTS `model_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `model_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `state` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `model_info`
+--
+
+LOCK TABLES `model_info` WRITE;
+/*!40000 ALTER TABLE `model_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `model_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `package_info`
+--
+
+DROP TABLE IF EXISTS `package_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `package_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `pid` varchar(1000) DEFAULT NULL,
+  `sid` varchar(1000) DEFAULT NULL,
+  `child_count` int(11) DEFAULT NULL,
+  `director_id` int(11) DEFAULT NULL,
+  `extra_attributes` mediumtext,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package_info`
+--
+
+LOCK TABLES `package_info` WRITE;
+/*!40000 ALTER TABLE `package_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `package_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `package_version`
+--
+
+DROP TABLE IF EXISTS `package_version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `package_version` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `submit_time` date DEFAULT NULL,
+  `package_id` int(11) NOT NULL,
+  `version_id` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `package_version`
+--
+
+LOCK TABLES `package_version` WRITE;
+/*!40000 ALTER TABLE `package_version` DISABLE KEYS */;
+/*!40000 ALTER TABLE `package_version` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_structure`
+--
+
+DROP TABLE IF EXISTS `staff_structure`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `staff_structure` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_structure`
+--
+
+LOCK TABLES `staff_structure` WRITE;
+/*!40000 ALTER TABLE `staff_structure` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff_structure` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_info`
+--
+
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `package_id` varchar(1000) DEFAULT NULL,
+  `role` int(11) DEFAULT '2',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_info`
+--
+
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+INSERT INTO `user_info` VALUES (1,'ligonghui','123456','23',2);
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-10-14 23:22:19
